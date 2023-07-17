a19c2fd47a66aeae1d7e38cba7d28b437b77d3ca
Kulpot <kulpotwahu@gmail.com>
2023-07-18 05:48:13 +0800

    Add project files.

---
+++ Threading DivideAndConquerExamplePart2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+//ref link:https://www.youtube.com/watch?v=5LU8WRL3xaE&list=PLRwVmtr-pp06KcX24ycbC-KkmAISAFKV5&index=12
+// Threading Divide and Conquer -- cut data to multiple threads one per CPU available then add it all
+//  8 core CPU ---
+
+class MainClass
+{
+    static byte[] values = new byte[500000000];
+    static long[] portionResults;
+    static int portionSize;
+    static void GenerateInts()
+    {
+        var rand = new Random(987);
+        for (int i = 0; i < values.Length; i++)
+            values[i] = (byte)rand.Next(10);
+    }
+    static void SumYourPortion(object portionNumber)
+    {
+        long sum = 0;
+        int portionNumberAsInt = (int)portionNumber;
+        for(int i = portionNumberAsInt * portionSize; 
+            i < portionNumberAsInt * portionSize + portionSize;
+            i++)
+        {
+            sum += values[i];
+        }
+        portionResults[portionNumberAsInt] = sum;
+    }
+    static void Main()
+    {
+        portionResults = new long[Environment.ProcessorCount];
+        portionSize = values.Length / Environment.ProcessorCount;
+        GenerateInts();
+        Console.WriteLine("Summing...");
+        Stopwatch watch = new Stopwatch();
+        watch.Start();
+        long total = 0;
+        for (int i = 0; i < values.Length; i++)
+            total += values[i];
+        watch.Stop();
+        Console.WriteLine("Total value is: " + total);
+        Console.WriteLine("Time to sum: " + watch.Elapsed);
+    }
+}
